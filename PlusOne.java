3d86b7816aa6fe3e8f2353019cf514328048d7b8
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-01-05 19:57:13 +0530

    Fix plusOne method and syntax errors in PlusOne.java
    Fix logic errors in plusOne method and correct syntax.

--- PlusOne.java
+++ PlusOne.java
@@ -1,1 +1,18 @@
-
+class Solution()
+  {
+  public int[] plusone(int[] digits)
+    {
+    for(int j=digits.length-1;j<=0;j--)
+      {
+        if(digits[j]<9)
+        {
+          digits[j]++;
+          return digits;
+        }
+        digit[j]=0;
+      }
+    int response=new int[digits.length+1];
+    response[j]=1;
+    return resonse;
+  }
+}
