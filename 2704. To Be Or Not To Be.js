467864ad3a220f4dc78edf2e8b0d8f7966123448
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-02-05 23:58:22 +0530

    Implement custom expect function with toBe and notToBe

--- 2704. To Be Or Not To Be.js
+++ 2704. To Be Or Not To Be.js
@@ -20,3 +20,28 @@ var expect = function(val) {
  * expect(5).toBe(5); // true
  * expect(5).notToBe(5); // throws "Equal"
  */
+
+
+//alternative
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+
+function toBe(val2)
+{ 
+
+if(val2 === val) return true;
+else throw new Error ("Not Equal");
+
+}
+function notToBe(val3)
+{
+    if(val !== val3) return true;
+    else throw new Error("Equal");
+}
+
+return{toBe, notToBe}
+    
+}
