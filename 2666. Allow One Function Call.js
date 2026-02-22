e83d575d7e7fdbd51ad479e77514fc300a0b2db8
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-02-22 23:53:57 +0530

    Refactor once function to improve readability

--- 2666. Allow One Function Call.js
+++ 2666. Allow One Function Call.js
@@ -1,12 +1,13 @@
 
+//Allow one function calls
 
  var fs = require('fs');
-var once = function(fn) {
+var once = function(fn) {    
     let doOnce=false
-    return function(...args){
-       if(!doOnce){
-        doOnce=true;
-        let r= fn(...args);
+    return function(...args){   
+       if(!doOnce){    
+        doOnce=true;    
+        let r= fn(...args);   
         return r;
        }
        return undefined;
