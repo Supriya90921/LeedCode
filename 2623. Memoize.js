b2ea9daa00afeaa8df745fc112c58638204ef8df
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-03-06 23:37:03 +0530

    Add memoization function for caching results
    Implement a memoization function to cache results.

--- 2623. Memoize.js
+++ 2623. Memoize.js
@@ -1,1 +1,16 @@
+function Memorize(fn)
+{
+  const cache={};
 
+  function Memorize(...args)
+  {
+    const key=JSON.stringify(args);  //convert args array into string
+    if(key in cache)
+    {
+      return cache[key];
+
+    }
+    cache[key]=fn(...args);
+    return cache[key];
+  }
+}
