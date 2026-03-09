eb09122fb248b380497328d9732ae742868a0647
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-03-09 23:57:40 +0530

    Implement cancellable timeout function
    Added a cancellable function that executes a callback after a timeout and can be cancelled.

--- 2715. Timeout Cancellation.js
+++ 2715. Timeout Cancellation.js
@@ -1,1 +1,20 @@
 
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+ var fs = require('fs');
+var cancellable = function(fn, args, t) {
+    const cancellable = function(){
+        clearTimeout(timer);
+    };
+    const timer=setTimeout(()=>{
+        fn(...args)
+    }, t);
+    return cancellable;
+    
+};
+fs.writeFileSync("display_runtime.txt", "0")
+
