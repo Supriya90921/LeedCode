f71fbf9bb9b0765a620cb740f68cc81e2c916cfa
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-03-18 23:53:42 +0530

    Implement cancellable function with interval execution
    Added a cancellable function that executes a callback at intervals and a file write operation.

--- 2725. Interval Cancellation.js
+++ 2725. Interval Cancellation.js
@@ -1,1 +1,9 @@
 
+var fs=require('fs');
+var cancellable = function(fn, args, t) {
+  fn(...args);
+ let timer = setInterval(()=> fn(...args));
+  let cancelFn= () =>clearInterval(timer);
+  return cancelFn;
+};
+fs.writeFileSync("display_runtime.txt", "0")
