0668ec5d347f3ed4900e308b915b8c3a58e9943e
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-03-24 00:00:28 +0530

    Correct removeDuplicates function implementation
    Fix syntax errors and improve the removeDuplicates function.

--- 26. Remove Duplicates from Sorted Array.java
+++ 26. Remove Duplicates from Sorted Array.java
@@ -1,1 +1,14 @@
 
+var removeDuplicates = function(nums) {
+  int k=1;
+
+  for(int i=1;i<nums.length;i++)
+    {
+    if(nums[k]!= nums[i-1];
+
+       {
+       nums[k]=nums[i];
+         k++;
+    }
+      return k;
+}
