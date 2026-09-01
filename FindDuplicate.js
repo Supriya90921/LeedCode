b71230bb1ddd8200d8753ce3964d487017d4c278
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-09-02 00:50:51 +0530

    Refactor findDuplicate function to accept array

--- FindDuplicate.js
+++ FindDuplicate.js
@@ -25,3 +25,36 @@ function findDuplicate(... nums)
 
 }
 console.log(findDuplicate(1,2,2,3));
+
+
+
+
+
+//without rest operattor
+
+function findDuplicate(nums)
+{
+
+
+  let seen = new Set();
+  let duplicate = new Set();
+
+  for (const num of nums)
+  {
+    if(seen.has(num))
+    {
+      duplicate.add(num);
+    }
+    else
+    {
+      seen.add(num);
+
+    }
+  }
+
+
+
+  return Array.from(duplicate);
+
+}
+console.log(findDuplicate([1,2,2,3]));
